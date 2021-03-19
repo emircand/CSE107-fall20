05950fbe82c4017cd8130b216ab72cace12642a9
Emircan Demirel <56741639+emircand@users.noreply.github.com>
2021-03-19 18:35:27 +0300

    Add files via upload

---
+++ Lab01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Emircan Demirel 1901042674 */
+
+int main(int argc, char *argv[]) {
+	int age= 45;
+	int course= 107;
+
+	printf("hello, my name is Emircan Demirel.\n");
+	printf("i am %d years old. This course is: %d", age, course);
+	
+	return 0;
+}
